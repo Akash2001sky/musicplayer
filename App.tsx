6ae42b21ab00c2c705c95578298103772ffcba73
Akash <akashrebala@gmail.com>
2023-04-13 19:53:46 +0530

    origin

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import { Text, View ,Appearance} from 'react-native'
+import React, { Component } from 'react'
+import Musicplayer from './components/Musicplayer'
+
+export class App extends Component {
+  render() {
+    return (
+
+    <Musicplayer/>
+    
+    )
+  }
+}
+
+export default App
